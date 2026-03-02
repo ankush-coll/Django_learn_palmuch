e3977300dfbff9581c9be0b0c238968b21d132bd
ankush-coll <ankushxavi@gmail.com>
2026-03-02 21:50:59 +0530

    second commitment

--- palmuch_app/settings.py
+++ palmuch_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-f%mqsjp2o&%ht9xae7&+oq^x3ir09(8dpo&dh3a9k2alhzu)(p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

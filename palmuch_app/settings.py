d48e6b104e5eed6c6fdfd0f40831807dd09a2319
ankush-coll <ankushxavi@gmail.com>
2026-03-03 21:12:47 +0530

    updated code for settings

--- palmuch_app/settings.py
+++ palmuch_app/settings.py
@@ -26,7 +26,7 @@ print(BASE_DIR)
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f%mqsjp2o&%ht9xae7&+oq^x3ir09(8dpo&dh3a9k2alhzu)(p'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

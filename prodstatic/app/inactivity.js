7700fbb20afe94cefb8ab2e2de106d246a72be78
ankush-coll <ankushxavi@gmail.com>
2026-03-07 23:27:12 +0530

    added logout alert-2

--- prodstatic/app/inactivity.js
+++ prodstatic/app/inactivity.js
@@ -4,16 +4,29 @@ let logoutTime = 2 * 60 * 1000;
 let warningTimer;
 let logoutTimer;
 
+function showWarningToast() {
+
+    Toastify({
+        text: "⚠️ You will be logged out in 1 minute due to inactivity",
+        duration: 60000,
+        gravity: "top",
+        position: "right",
+        backgroundColor: "#ff9800",
+        close: true
+    }).showToast();
+
+}
+
 function resetTimers() {
 
     clearTimeout(warningTimer);
     clearTimeout(logoutTimer);
 
-    warningTimer = setTimeout(function() {
-        alert("You will be logged out in 1 minute due to inactivity.");
+    warningTimer = setTimeout(function () {
+        showWarningToast();
     }, warningTime);
 
-    logoutTimer = setTimeout(function() {
+    logoutTimer = setTimeout(function () {
         window.location.href = "/accounts/login/";
     }, logoutTime);
 }
@@ -21,5 +34,6 @@ function resetTimers() {
 window.onload = resetTimers;
 
 document.onmousemove = resetTimers;
+document.onkeypress = resetTimers;
 document.onclick = resetTimers;
 document.onscroll = resetTimers;
